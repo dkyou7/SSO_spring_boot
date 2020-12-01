29bba5481db448b14f4e2f1e4da1d830cfeba1a6
youdongkwan <dkyou7@naver.com>
2020-12-01 15:15:43 +0900

    DB 토큰을 통한 Resource 요청

---
+++ testRes1/src/main/java/com/ktnet/testClient1/user/UserController.java
@@ -0,0 +1,24 @@
+package com.ktnet.testClient1.user;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+ 
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/v1")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping(value = "/users")
+    public List<User> findAllUser() {
+        return userRepository.findAll();
+    }
+}
